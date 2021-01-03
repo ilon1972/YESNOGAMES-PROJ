222bd03ad474f11cc00500631877086079e5fc5e
ilon1972 <56972296+ilon1972@users.noreply.github.com>
2021-01-03 19:15:19 +0200

    Working prototype

---
+++ yesnogame/flip.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $('.flip-answer1').click(function () {
+    $('.first-flip-container').toggleClass("flipped");
+  });
+  $('.flip-answer2').click(function () {
+    $('.second-flip-container').toggleClass("flipped");
+  });
+  $('.flip-answer3').click(function () {
+    $('.third-flip-container').toggleClass("flipped");
+  });
+  $('.flip-answer4').click(function () {
+    $('.fourth-flip-container').toggleClass("flipped");
+  });
+  $('.flip-answer5').click(function () {
+    $('.fifth-flip-container').toggleClass("flipped");
+  });
+  $('.flip-answer6').click(function () {
+    $('.sixth-flip-container').toggleClass("flipped");
+  });
+  $('.flip-answer7').click(function () {
+    $('.seventh-flip-container').toggleClass("flipped");
+  });
+  $('.flip-answer8').click(function () {
+    $('.eighth-flip-container').toggleClass("flipped");
+  });
+  $('.flip-answer9').click(function () {
+    $('.nineth-flip-container').toggleClass("flipped");
+  });
+});
